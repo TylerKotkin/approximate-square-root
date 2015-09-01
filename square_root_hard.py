a35166619006c8cf056403fd27cea905b319df1d
Tyler Kotkin <Tylerkotkin@gmail.com>
2015-09-01 16:03:48 -0400

    changes made to hard

--- square_root_hard.py
+++ square_root_hard.py
@@ -1,10 +1,17 @@
 def squareroot():
     y = 1
-    x = int(input("Please enter a even number higher than 0: "))
-    while round(((y*y)-x), 4) != 0:
-        y = ((y+(x/y))/2)
+    x = int(input("Please enter an even number higher than 0: "))
+    if type(x) != int:
+         print("That is not an even number higher than 0")
+         exit()
+    elif x <= 0:
+        print("That is not an even number higher than 0")
+        exit()
+    else:
+        while round(((y*y)-x), 4) != 0:
+            y = ((y+(x/y))/2)
 
-        #print(y)
-    #return print(y)
+            print(y)
+
     print("The squareroot of {} is {}".format(x,y))
 squareroot()
