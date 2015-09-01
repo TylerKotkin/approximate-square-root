5aa6886d99cb7c5e6678a8f017475f06c2e2504c
Tyler Kotkin <Tylerkotkin@gmail.com>
2015-09-01 17:12:43 -0400

    added guess input

--- square_root_hard.py
+++ square_root_hard.py
@@ -2,12 +2,15 @@ def squareroot():
 
     try:
         x = int(input("Please enter a number higher than 0: "))
-        y = 1
+        y = int(input("Please guess the square root of the number entered: "))
 
     except ValueError:
-         print("ERROR: That is not a number higher than 0")
+         print("ERROR: That is not a number")
          exit()
     if int(x) <= 0:
+        print("ERROR: That is not a number higher than 0")
+        exit()
+    if int(y) <= 0:
         print("ERROR: That is not a number higher than 0")
         exit()
     else:
