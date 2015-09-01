92c946aa23bef0c253ca07b540867ec66929c2ee
Tyler Kotkin <Tylerkotkin@gmail.com>
2015-09-01 16:01:37 -0400

    added guess input

--- square_root.py
+++ square_root.py
@@ -1,12 +1,11 @@
 
 
 def squareroot():
-    y = 1
     x = int(input("Please enter a even number higher than 0: "))
+    y = int(input("Please guess the square root of the number entered: "))
     while round(((y*y)-x), 4) != 0:
         y = ((y+(x/y))/2)
 
-        #print(y)
-    #return print(y)
+        
     print("The squareroot of {} is {}".format(x,y))
 squareroot()
