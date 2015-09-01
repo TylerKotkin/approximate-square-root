d9bad20b26a98cefb16663970c7f5852fea029bd
Tyler Kotkin <Tylerkotkin@gmail.com>
2015-09-01 15:22:50 -0400

    created square_root_hard.py

---
+++ square_root_hard.py
@@ -0,0 +1,10 @@
+def squareroot():
+    y = 1
+    x = int(input("Please enter a even number higher than 0: "))
+    while round(((y*y)-x), 4) != 0:
+        y = ((y+(x/y))/2)
+
+        #print(y)
+    #return print(y)
+    print("The squareroot of {} is {}".format(x,y))
+squareroot()
