b4f9449c6abd1e9bef832e72dbaaf4715157b5aa
Tyler Kotkin <Tylerkotkin@gmail.com>
2015-09-01 16:28:08 -0400

    fixed input question

--- square_root.py
+++ square_root.py
@@ -1,11 +1,11 @@
 
 
 def squareroot():
-    x = int(input("Please enter a even number higher than 0: "))
+    x = int(input("Please enter a number higher than 0: "))
     y = int(input("Please guess the square root of the number entered: "))
     while round(((y*y)-x), 4) != 0:
         y = ((y+(x/y))/2)
 
-        
+
     print("The squareroot of {} is {}".format(x,y))
 squareroot()
